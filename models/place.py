a19507dc66201160c14c65d19ba8e6ca65e6b075
Gustavo Jomeque Maruyama <32277272+GustavoTechVR@users.noreply.github.com>
2024-01-07 22:58:57 -0500

    Update city.py place.py

--- models/place.py
+++ models/place.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
-
 place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
                              ForeignKey("places.id"),
@@ -18,7 +17,6 @@ place_amenity = Table("place_amenity", Base.metadata,
                              primary_key=True,
                              nullable=False))
 
-
 class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
@@ -26,11 +24,11 @@ class Place(BaseModel, Base):
         user_id: user id
         name: name input
         description: string of description
-        number_rooms: number of room in int
+        number_rooms: number of rooms in int
         number_bathrooms: number of bathrooms in int
         max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
-        latitude: latitude in flaot
+        price_by_night: price for a stay in int
+        latitude: latitude in float
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
