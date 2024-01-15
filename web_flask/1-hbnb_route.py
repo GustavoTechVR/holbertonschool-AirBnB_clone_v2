d778d06089f351c7f8ef19eedca31982131841f2
Gustavo Jomeque Maruyama <32277272+GustavoTechVR@users.noreply.github.com>
2024-01-14 23:56:43 -0500

    Feat: added 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+1-hbnb_route.py: Starts a Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display 'Hello HBNB!' when the root URL is accessed.
+
+    Returns:
+        str: The message 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Display 'HBNB' when the '/hbnb' URL is accessed.
+
+    Returns:
+        str: The message 'HBNB'.
+    """
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
