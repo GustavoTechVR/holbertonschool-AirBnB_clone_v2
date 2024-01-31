3ad72a441908d5b3c95d6fe7325f60f14ecf8087
Gustavo Jomeque Maruyama <32277272+GustavoTechVR@users.noreply.github.com>
2024-01-30 20:28:57 -0500

    Fix: update 6-number_odd_or_even.py 6-number_odd_or_even.html

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -17,6 +18,7 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """
@@ -26,6 +28,7 @@ def display_hbnb():
         str: The message 'HBNB'.
     """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_custom_text(text):
@@ -39,6 +42,7 @@ def display_custom_text(text):
         str: The message 'C ' followed by the value of the text variable.
     """
     return "C " + text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -54,6 +58,7 @@ def display_python_text(text):
     """
     return "Python " + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """
@@ -67,6 +72,7 @@ def display_number(n):
     """
     return f"{n} is a number"
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def display_number_odd_or_even(n):
     """
@@ -79,6 +85,7 @@ def display_number_odd_or_even(n):
         str: The HTML page indicating whether n is even or odd.
     """
     return render_template('6-number_odd_or_even.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
