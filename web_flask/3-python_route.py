891b9498205be48b2702c879c03ea0c51e229b43
Gustavo Jomeque Maruyama <32277272+GustavoTechVR@users.noreply.github.com>
2024-01-30 20:17:00 -0500

    Feat: update 3-python_route.py v2

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -17,6 +18,7 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """
@@ -26,6 +28,7 @@ def display_hbnb():
         str: The message 'HBNB'.
     """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_custom_text(text):
@@ -40,6 +43,7 @@ def display_custom_text(text):
     """
     return "C " + text.replace('_', ' ')
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python_text(text):
@@ -53,6 +57,7 @@ def display_python_text(text):
         str: The message 'Python ' followed by the value of the text variable.
     """
     return "Python " + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
