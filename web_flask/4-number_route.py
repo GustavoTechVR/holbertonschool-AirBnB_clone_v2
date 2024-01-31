4fb0c76cecf5cdda3afc91faa086c88c5ce14e6b
Gustavo Jomeque Maruyama <32277272+GustavoTechVR@users.noreply.github.com>
2024-01-30 20:18:04 -0500

    Feat: update 4-number_route.py v2

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -17,6 +18,7 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """
@@ -26,6 +28,7 @@ def display_hbnb():
         str: The message 'HBNB'.
     """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_custom_text(text):
@@ -39,6 +42,7 @@ def display_custom_text(text):
         str: The message 'C ' followed by the value of the text variable.
     """
     return "C " + text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -54,6 +58,7 @@ def display_python_text(text):
     """
     return "Python " + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """
@@ -66,6 +71,7 @@ def display_number(n):
         str: The message '<n> is a number' if n is an integer.
     """
     return f"{n} is a number"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
